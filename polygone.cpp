338a6f000f7144b0bbe41ca1c7084b444a228188
Dave <david92galbraith@gmail.com>
2014-04-18 00:54:18 -0700

    SHIP THIS BITCH

--- polygone.cpp
+++ polygone.cpp
@@ -26,6 +26,8 @@
 #include <fstream>
 #include <string.h>
 #define PI 3.14159265  // Should be used from mathlib
+#define randy ((float) (rand() % 1000000000) / 10000000000000)
+//#define randy 0
 
 using namespace std;
 //camera crap copeid from Rob Bateman http://nccastaff.bournemouth.ac.uk/jmacey/RobTheBloke/www/opengl_programming.html#4
@@ -48,6 +50,7 @@ float ty = 0;
 int lastx=0;
 int lasty=0;
 int wireframe = 0;
+int adaptive;
 
 float lookatx = 0.0;
 float lookaty = 0.0;
@@ -74,13 +77,6 @@ void keyPressed (unsigned char key, int x, int y);
 void arrows(int key, int x, int y);
 
 //****************************************************
-// Simple init function
-//****************************************************
-
-void initScene(){
-  // Nothing to do here for this simple example.
-}
-//****************************************************
 // reshape viewport if the window is resized
 //****************************************************
 
@@ -88,17 +84,25 @@ void myReshape(int w, int h) {
   viewport.w = w;
   viewport.h = h;
   glViewport (0,0,viewport.w,viewport.h);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glutPostRedisplay();
+
 }
 //****************************************************
 // function that does the actual drawing of stuff
 //***************************************************
+float colorx = 1.5;
+float colory = -1.5;
+float colorz = 0.5;
 
 void uniformDisplay() {
-  glClear(GL_COLOR_BUFFER_BIT);				// clear the color buffer
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);				// clear the color buffer
+  glEnable(GL_DEPTH_TEST);
+
   glMatrixMode(GL_MODELVIEW);			        // indicate we are specifying camera transformations
   glLoadIdentity();
-  gluLookAt(camerax, cameray, cameraz, lookatx, lookaty, lookatz, 0, 0, 1);
-
+  //gluLookAt(camerax, cameray, cameraz, lookatx, lookaty, lookatz, 0, 0, 1);
+  gluLookAt(camerax, cameray, cameraz, 0, 0, 0, 0, 0, 1);
   //glLoadIdentity();
 
   /*glTranslatef(0,-zoom,0);*/
@@ -107,7 +111,6 @@ void uniformDisplay() {
   glRotatef(roty,1,0,0);
   // Start drawing
   if (flat) {
-    // glColor3f(1.0f, 1.0f, 1.0f);
     glShadeModel(GL_FLAT);
   } else {
     glShadeModel(GL_SMOOTH);
@@ -115,26 +118,39 @@ void uniformDisplay() {
     GLfloat diffuse0[]={1.0, 0.0, 0.0, 1.0};
     GLfloat ambient0[]={0.0, 0.0, 0.0, 1.0};
     GLfloat specular0[]={1.0, 1.0, 1.0, 1.0};
-    GLfloat light0_pos[]={0.0, 0.0, 1.0, 1.0};
+    GLfloat light0_pos[]={colorx, colory, colorz, 1.0};
+    GLfloat diffuse1[]={1.0, 0.0, 0.0, 1.0};
+    GLfloat ambient1[]={0.0, 0.0, 0.0, 1.0};
+    GLfloat specular1[]={1.0, 1.0, 1.0, 1.0};
+    GLfloat light1_pos[]={camerax, cameray, cameraz, 1.0};
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glLightfv(GL_LIGHT0, GL_POSITION, light0_pos);
     glLightfv(GL_LIGHT0, GL_AMBIENT, ambient0);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse0);
     glLightfv(GL_LIGHT0, GL_SPECULAR, specular0);
+    glEnable(GL_LIGHT1);
+    glLightfv(GL_LIGHT1, GL_POSITION, light1_pos);
+    glLightfv(GL_LIGHT1, GL_AMBIENT, ambient1);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse1);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, specular1);
     GLfloat ambient[] = {1.0, 0.0, 0.0, 1.0};
     GLfloat diffuse[] = {1.0, 0.8, 0.0, 1.0};
     GLfloat specular[] = {1.0, 1.0, 1.0, 1.0};
     GLfloat shine = 100.0;
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
-    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shine); 
+    /*glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+      glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+      glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+      glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shine); */
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+    glMaterialf(GL_FRONT, GL_SHININESS, shine); 
   }
-  glPointSize(50.0f);
-  //glBegin(GL_POINTS);
-  //glVertex3f(0, 0, 0);
-  //glEnd();
+  glPointSize(10.0f);
+  glBegin(GL_POINTS);
+  glVertex3f(colorx, colory, colorz);
+  glEnd();
   for (int patch = 0; patch < patches.size(); patch++) {
     //for (int patch = 0; patch < 1; patch++) {
     vector<vector<vector<float> > > currPatch = patches[patch];
@@ -171,7 +187,13 @@ void uniformDisplay() {
 	glEnd();
 	/*glBegin(GL_LINES);
 	  glVertex3fv(floady(interp1[0]));
-	  glVertex3fv(floady(pluss(interp1[0], times(interp1[1], 0.1))));
+	  glVertex3fv(floady(pluss(interp1[0], times(interp1[1], 0.15))));
+	  glVertex3fv(floady(interp2[0]));
+	  glVertex3fv(floady(pluss(interp2[0], times(interp2[1], 0.15))));
+	  glVertex3fv(floady(interp3[0]));
+	  glVertex3fv(floady(pluss(interp3[0], times(interp3[1], 0.15))));	
+	  glVertex3fv(floady(interp4[0]));
+	  glVertex3fv(floady(pluss(interp4[0], times(interp4[1], 0.15))));
 	  glEnd();*/
       }
     }
@@ -181,71 +203,6 @@ void uniformDisplay() {
 }
 
 void adaptiveDisplay() {
-  glClear(GL_COLOR_BUFFER_BIT);				// clear the color buffer
-  glMatrixMode(GL_MODELVIEW);			        // indicate we are specifying camera transformations
-  glLoadIdentity();
-  gluLookAt(.2, .1, 0.5, 0.5, 0.5, 0.7, 0, 0, 1);
-  
-  // Start drawing
-  if (flat) {
-    glColor3f(1.0f, 1.0f, 1.0f);
-  } else {
-    glShadeModel(GL_SMOOTH);
-    //copied from http://www.cs.uml.edu/~haim/teaching/cg/resources/presentations/427/AngelCG20_shading_OpenGL.pdf.
-    GLfloat diffuse0[]={1.0, 0.0, 0.0, 1.0};
-    GLfloat ambient0[]={0.0, 0.0, 0.0, 1.0};
-    GLfloat specular0[]={1.0, 1.0, 1.0, 1.0};
-    GLfloat light0_pos[]={0.0, 0.0, 1.0, 1.0};
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    glLightfv(GL_LIGHT0, GL_POSITION, light0_pos);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient0);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse0);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, specular0);
-    GLfloat ambient[] = {1.0, 0.0, 0.0, 1.0};
-    GLfloat diffuse[] = {1.0, 0.8, 0.0, 1.0};
-    GLfloat specular[] = {1.0, 1.0, 1.0, 1.0};
-    GLfloat shine = 100.0;
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
-    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shine); 
-  }
-  glPointSize(50.0f);
-  //glBegin(GL_POINTS);
-  //glVertex3f(0, 0, 0);
-  //glEnd();
-  for (int patch = 0; patch < patches.size(); patch++) {
-    //for (int patch = 0; patch < 1; patch++) {
-    vector<vector<vector<float> > > currPatch = patches[patch];
-    int numdiv = (1 + step/10) / step;
-    for (int iu = 0; iu < numdiv; iu++) {
-      float u = iu * step;
-      for (int iv = 0; iv < numdiv; iv++) {
-	float v = iv * step;
-	vector<vector<float> >interp1 = beezerpatch(currPatch, u, v);
-	vector<vector<float> >interp2 = beezerpatch(currPatch, u+step, v);
-	vector<vector<float> >interp3 = beezerpatch(currPatch, u, v+step);
-	vector<vector<float> >interp4 = beezerpatch(currPatch, u+step, v+step);
-	glBegin(GL_QUADS);
-	glNormal3fv(floady(interp1[1]));
-	glVertex3fv(floady(interp1[0]));
-	glNormal3fv(floady(interp2[1]));
-	glVertex3fv(floady(interp2[0]));
-	glNormal3fv(floady(interp4[1]));
-	glVertex3fv(floady(interp4[0]));
-	glNormal3fv(floady(interp3[1]));
-	glVertex3fv(floady(interp3[0]));
-	glEnd();
-	/*glBegin(GL_LINES);
-	  glVertex3fv(floady(interp1[0]));
-	  glVertex3fv(floady(pluss(interp1[0], times(interp1[1], 0.1))));
-	  glEnd();*/
-      }
-    }
-  }
-  glFlush();
-  glutSwapBuffers();			// swap buffers (we earlier set float buffer)
 }
 
 GLfloat* floady(vector<float> x) {
@@ -265,14 +222,14 @@ vector<vector<float> > beezercurve(vector<vector<float> > curve, float u) {
   vector<float> e = pluss(times(b, 1-u), times(c, u));
   vector<float> p = pluss(times(d, 1-u), times(e, u));
   vector<float> dpdu = times(minuss(e, d), 3);
-  if (dpdu[0] == 0 && dpdu[1] == 0 && dpdu[2] == 0) {
+  /*if (dpdu[0] == 0 && dpdu[1] == 0 && dpdu[2] == 0) {
     cout << "Horrror\n";
     printvect(e);
     printvect(d);
     cout << u << "\n";
     printvectvect(curve);
     cout << "/horror\n\n";
-  }
+    }*/
   vector<vector<float> > result;
   result.push_back(p);
   result.push_back(dpdu);
@@ -284,10 +241,10 @@ vector<vector<float> >beezerpatch(vector<vector<vector<float> > > patch, float u
   vector<vector<float> > draw;
   vector<vector<float> > ucurve;
   vector<vector<float> > vcurve;
-  vcurve.push_back(beezercurve(patch[0], u)[0]);
-  vcurve.push_back(beezercurve(patch[1], u)[0]);
-  vcurve.push_back(beezercurve(patch[2], u)[0]);
-  vcurve.push_back(beezercurve(patch[3], u)[0]);
+  vcurve.push_back(beezercurve(patch[0], u+randy)[0]);
+  vcurve.push_back(beezercurve(patch[1], u+randy)[0]);
+  vcurve.push_back(beezercurve(patch[2], u+randy)[0]);
+  vcurve.push_back(beezercurve(patch[3], u+randy)[0]);
   vector<vector<float> > col1;
   vector<vector<float> > col2;
   vector<vector<float> > col3;
@@ -308,16 +265,18 @@ vector<vector<float> >beezerpatch(vector<vector<vector<float> > > patch, float u
   col4.push_back(patch[1][3]);
   col4.push_back(patch[2][3]);
   col4.push_back(patch[3][3]);
-  ucurve.push_back(beezercurve(col1, v)[0]);
-  ucurve.push_back(beezercurve(col2, v)[0]);
-  ucurve.push_back(beezercurve(col3, v)[0]);
-  ucurve.push_back(beezercurve(col4, v)[0]);
-  cout << "V";
+  ucurve.push_back(beezercurve(col1, v+randy)[0]);
+  ucurve.push_back(beezercurve(col2, v+randy)[0]);
+  ucurve.push_back(beezercurve(col3, v+randy)[0]);
+  ucurve.push_back(beezercurve(col4, v+randy)[0]);
+  ///cout << "V";
   vector<vector<float> > pdpdv = beezercurve(vcurve, v);
-  cout << "U";
+  ///cout << "U";
   vector<vector<float> > pdpdu = beezercurve(ucurve, u);
-  cout << "free\n";
+  ///cout << "free\n";
   vector<float> n = normalize(cross(pdpdu[1], pdpdv[1]));
+  //cout << "lol";
+  //printvect(n);
   if (n[0] == 0 && n[1] == 0 && n[2] == 0) {
     cout << "Diagnostics\n\n";
     cout << "Patch:";
@@ -345,8 +304,38 @@ vector<vector<float> >beezerpatch(vector<vector<vector<float> > > patch, float u
   return draw;
 }
 
+void initScene(int argc, char *argv[]) {
+  glutInit(&argc, argv);
+  //This tells glut to use a float-buffered window with red, green, and blue channels 
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
+  // Initalize theviewport size
+  viewport.w = 400;
+  viewport.h = 400;
+  //The size and position of the window
+  glutInitWindowSize(viewport.w, viewport.h);
+  glutInitWindowPosition(0,0);
+  glutCreateWindow(argv[0]);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  if (adaptive) {						// quick function to set up scene
+    glutDisplayFunc(adaptiveDisplay);
+  } else {
+    glutDisplayFunc(uniformDisplay); // function to run when its time to draw something
+  }
+  glutReshapeFunc(myReshape);				// function to run when the window gets resized
+  glutKeyboardFunc(keyPressed); // Tell GLUT to use the method "keyPressed" for key presses (http://www.swiftless.com/tutorials/opengl/keyboard.html)
+  glutSpecialFunc(arrows);
+  gluPerspective(45, 1, 0.01, 10);
+  camerax = lookatx/2;
+  cameray = 2 * (lookatx + lookaty + lookatz);
+  cameraz = 8;
+  cout << camerax << " " << cameray << " " << cameraz;
+}
+
 int main(int argc, char *argv[]) {
-  int adaptive = 0;
+  adaptive = 0;
   if (argc > 3) {
     if (strlen(argv[3]) == 2) {
       if (isEqual(argv[3], "-a", 2)) {
@@ -391,31 +380,9 @@ int main(int argc, char *argv[]) {
   lookatz /= (16.0 * paches);
   cout << lookatx << " " << lookaty << " " << lookatz << "\n";
   //This initializes glut
-  glutInit(&argc, argv);
-  //This tells glut to use a float-buffered window with red, green, and blue channels 
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-  // Initalize theviewport size
-  viewport.w = 400;
-  viewport.h = 400;
-  //The size and position of the window
-  glutInitWindowSize(viewport.w, viewport.h);
-  glutInitWindowPosition(0,0);
-  glutCreateWindow(argv[0]);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  initScene();
-  if (adaptive) {						// quick function to set up scene
-    glutDisplayFunc(adaptiveDisplay);
-  } else {
-    glutDisplayFunc(uniformDisplay); // function to run when its time to draw something
-  }
-  glutReshapeFunc(myReshape);				// function to run when the window gets resized
-  glutKeyboardFunc(keyPressed); // Tell GLUT to use the method "keyPressed" for key presses (http://www.swiftless.com/tutorials/opengl/keyboard.html)
-  glutSpecialFunc(arrows);
-  gluPerspective(45, 1, 0.01, 10);
-  camerax = lookatx/2;
-  cameray = 2 * (lookatx + lookaty + lookatz);
-  cameraz = lookatz * 4;
+ 
+  initScene(argc, argv);
+ 
   glutMainLoop();							// infinite loop that will keep drawing and resizing
   // and whatever else
   return 0;
@@ -424,7 +391,6 @@ int main(int argc, char *argv[]) {
 void arrows(int key, int x, int y) {
   int shift = glutGetModifiers();
   if (shift == GLUT_ACTIVE_SHIFT) {
-    cout << "Shifty bastard.\n";
     if (key == GLUT_KEY_UP) {
       ty += 0.1;
     }
@@ -438,7 +404,6 @@ void arrows(int key, int x, int y) {
       tx -= 0.1;
     }
   } else {
-    cout << "No shift\n";
     if (key == GLUT_KEY_UP) {
       roty += 1;
     }
@@ -508,9 +473,9 @@ vector<float> cross(vector<float> u, vector<float> v) {
 }
 
 vector<float> normalize(vector<float> v) {
-  //  cout << "normalizen: ";
+  //cout << "normalizen: ";
   //printvect(v);
-  float norm;
+  float norm = 0;
   vector<float> result;
   for (int k = 0; k < v.size(); k++) {
     norm += v[k] * v[k];
@@ -519,6 +484,8 @@ vector<float> normalize(vector<float> v) {
     cout << "Tragedy: nomrmalized a zero\n";
     norm = 1;
   }
+  //cout << "\nnorm " << norm << "\n";
+  //printvect(v);
   norm = sqrt(norm);
   result.push_back(v[0] / norm);
   result.push_back(v[1] / norm);
@@ -533,6 +500,7 @@ void printate(GLfloat x[], int length) {
   for (int k = 0; k < length; k++) {
     cout << x[k] << ", ";
   }
+  cout << "\n";
 }
 
 void printvectarray(vector<vector<vector<float> > > x) {
@@ -546,6 +514,7 @@ void printvectarray(vector<vector<vector<float> > > x) {
       cout << "  ";
     }
   }
+  cout << "\n";
 }
 
 void printvectvect(vector<vector<float> > x) {
@@ -555,6 +524,7 @@ void printvectvect(vector<vector<float> > x) {
       cout << x[k][j] << " ";
     }
   }
+  cout << "\n";
 }
 
 void printvect(vector<float> x) {
